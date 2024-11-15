7753a4a01eb93496911393c65453248a25851e82
Douglas Berkley <douglasmberkley@gmail.com>
2024-11-15 16:34:59 +0900

    Update seeds.rb
    adding more pages

--- seeds.rb
+++ seeds.rb
@@ -3,17 +3,19 @@ Movie.destroy_all
 # List.destroy_all
 
 # the Le Wagon copy of the API
-url = 'http://tmdb.lewagon.com/movie/top_rated'
-response = JSON.parse(URI.open(url).read)
-
-response['results'].each do |movie_hash|
-  puts
-  p movie_hash
-  # create an instance with the hash
-  Movie.create!(
-    poster_url: "https://image.tmdb.org/t/p/w500" + movie_hash['poster_path'],
-    rating: movie_hash['vote_average'],
-    title: movie_hash['title'],
-    overview: movie_hash['overview']
-  )
+(1..5).to_a.each do |num|
+  url = "http://tmdb.lewagon.com/movie/top_rated?page=#{num}"
+  response = JSON.parse(URI.open(url).read)
+  
+  response['results'].each do |movie_hash|
+    puts
+    p movie_hash
+    # create an instance with the hash
+    Movie.create!(
+      poster_url: "https://image.tmdb.org/t/p/w500" + movie_hash['poster_path'],
+      rating: movie_hash['vote_average'],
+      title: movie_hash['title'],
+      overview: movie_hash['overview']
+    )
+  end
 end
