3ec885a8bb563573f9a898b830037a012d95a16e
Douglas Berkley <douglasmberkley@gmail.com>
2025-02-13 12:07:54 +0900

    Update seeds.rb
    Updated seed feedback

--- seeds.rb
+++ seeds.rb
@@ -1,15 +1,17 @@
 require 'open-uri'
+puts "Cleaning the DB...."
 Movie.destroy_all
 # List.destroy_all
 
 # the Le Wagon copy of the API
+puts "Creating movies.... \n"
 (1..5).to_a.each do |num|
   url = "http://tmdb.lewagon.com/movie/top_rated?page=#{num}"
   response = JSON.parse(URI.open(url).read)
   
   response['results'].each do |movie_hash|
+    puts "...creating the movie #{movie_hash['title']}..."
     puts
-    p movie_hash
     # create an instance with the hash
     Movie.create!(
       poster_url: "https://image.tmdb.org/t/p/w500" + movie_hash['poster_path'],
@@ -19,3 +21,4 @@ Movie.destroy_all
     )
   end
 end
+puts "... created #{Movie.count} movies."
